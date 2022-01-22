8fed079b409bc4fbe8a3f7f01df24b2eb2ba4d08
sadhanapatil16 <98214842+sadhanapatil16@users.noreply.github.com>
2022-01-22 07:18:28 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+<html>
+<head>
+
+    <title>Add Title Here</title>
+
+    <script type="text/javascript"
+	    src="https://public.tableau.com/app/profile/sadhana3355/viz/Capstone2_16421398685100/CreditCardDefaultPrediction?publish=yes"></script>
+    <script type="text/javascript"></script>
+
+</head>
+
+<body>
+
+  <h1>Add Title Here</h1>
+
+    <button id = "exportPDF">Generate PDF</button>
+    <button id = "exportImage">Generate Image</button>
+
+
+    <div id="vizContainer"></div>
+
+    <script src="./app.js"></script>
+
+</body>
+
+</html>
